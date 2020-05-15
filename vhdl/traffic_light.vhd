7a6e3402351b69759b047a7641696a08556bbd81
basicskill <mladenbasic99@gmail.com>
2020-05-15 15:53:09 +0200

    :ambulance: Time counting fix

--- vhdl/traffic_light.vhd
+++ vhdl/traffic_light.vhd
@@ -16,7 +16,8 @@ architecture Behavioral of traffic_light is
     );
 
     signal state_reg, next_state : TrafficState;
-    signal time_left : integer range 0 to 10;
+    signal time_left    : integer range 0 to 10;
+    signal new_time     : integer range 0 to 10; 
 
 begin
 
@@ -25,7 +26,13 @@ begin
         if rising_edge(clk) then
             if reset = '1' then
                 state_reg <= redRed1;   -- Reset state
+                time_left <= 4;
             else
+                if time_left = 0 then
+                    time_left <= new_time;
+                else
+                    time_left <= time_left - 1;
+                end if;
                 state_reg <= next_state;
             end if;
         end if;
@@ -33,25 +40,24 @@ begin
 
     NEXT_STATE_LOGIC : process( state_reg, time_left )
     begin
-        if time_left = 1 then -- PROVERI VREDNOST U SIMULATORU!
+        if time_left = 0 then -- PROVERI VREDNOST U SIMULATORU!
             case state_reg is
             
                 when redRed1 =>
                     next_state <= redGreen; -- !!!
-                    time_left <= 3; -- !!! Periodi clk vs Sekunde
+                    new_time <= 3; -- !!! Periodi clk vs Sekunde
                 
                 when redGreen =>
                     next_state <= redRed1; -- !!!
-                    time_left <= 4;
+                    new_time <= 4;
             
                 when others =>
                     next_state <= redRed1;
-                    time_left <= 4;
+                    new_time <= 4;
             
             end case;
         else
             next_state <= state_reg;
-            time_left <= time_left - 1;
         end if ; 
     end process ; -- NEXT_STATE_LOGIC
 
